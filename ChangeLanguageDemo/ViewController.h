70aa167f79f1cb2ba68aa13fa9e9c060e8042a91
KindDeath <1157756119@qq.com>
2016-07-13 09:49:12 +0800

    Initial Commit

---
+++ ChangeLanguageDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ChangeLanguageDemo
+//
+//  Created by lza on 16/7/13.
+//  Copyright © 2016年 lza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ChangeLanguageDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ChangeLanguageDemo
+//
+//  Created by lza on 16/7/13.
+//  Copyright © 2016年 lza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
